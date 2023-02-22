a94ea3758b057ed21abfdd54f18e3366c89531cc
Harsh Pandey <harshp6421@gmail.com>
2023-02-22 12:23:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const cardObjectDefinitions = [
-    {id:1, imagePath:'/images/card-KingHearts.png'},
-    {id:2, imagePath:'/images/card-JackClubs.png'},
-    {id:3, imagePath:'/images/card-QueenDiamonds.png'},
-    {id:4, imagePath:'/images/card-AceSpades.png'}
+    {id:1, imagePath:'./images/card-KingHearts.png'},
+    {id:2, imagePath:'./images/card-JackClubs.png'},
+    {id:3, imagePath:'./images/card-QueenDiamonds.png'},
+    {id:4, imagePath:'./images/card-AceSpades.png'}
 ]
 const aceId = 4
 
